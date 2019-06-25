7dbef4013d66b9b886383c1514fc14b56dfe9319
Bronwyn <bronwyn.vdb@gmail.com>
2019-06-25 21:23:56 +1000

    init

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+	//
+?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Responsive background video using Youtube API & javascript</title>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+		<!-- Import Font Awesome Css -->
+		<link rel="stylesheet" href="assets/fonts/font-awesome/css/font-awesome.css"/>
+
+		<link href="https://fonts.googleapis.com/css?family=Poppins:400,700" rel="stylesheet">
+
+		<link rel="stylesheet" href="styles.css" />
+
+	</head>
+	<body>
+
+	</body>
+</html>
